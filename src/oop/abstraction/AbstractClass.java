628cad6b594d1f8f6c513bcc96a152c101acbec3
milanthapaliya <thapaliyamilan8@gmail.com>
2023-08-18 21:12:18 +0545

    added interface

--- src/oop/abstraction/AbstractClass.java
+++ src/oop/abstraction/AbstractClass.java
@@ -4,8 +4,9 @@ package oop.abstraction;
 /*
     1. Abstraction is the process of hiding the unnecessary complexity from the user
     2. Some times cases arises where methods are mandatory to be implemented by subclasses
-    3. Abstracts methods are methods without body and they can only be implemented inside abstract class
-    4. An abstract method is a method that is declared but not defined in a class. It acts as a placeholder for methods that must be implemented in subclasses
+    3. Abstract Class cannot be instantiated, but it can be subclassed
+    4. Abstract methods are methods without body and they can only be implemented inside abstract class
+    5. An abstract method is a method that is declared but not defined in a class. It acts as a placeholder for methods that must be implemented in subclasses
  */
 
 
@@ -20,11 +21,14 @@ public class AbstractClass {
         car.accelerate();
         bike.accelerate();
         System.out.println("Car Speed is " + car.getSpeed() + " Bike speed is " + bike.getSpeed()); //Car Speed is 2 Bike speed is 4
+        car.decelerate();
+        bike.decelerate();
+        System.out.println("Car Speed is " + car.getSpeed() + " Bike speed is " + bike.getSpeed()); //Car Speed is 1 Bike speed is 3
     }
 
 
 }
-abstract class Accelerate{
+abstract class Vehicle {
     protected int speed = 0;
 
     public abstract void accelerate();
@@ -32,15 +36,19 @@ abstract class Accelerate{
     public int getSpeed() {
         return speed;
     }
+
+    public void decelerate(){
+        speed--;
+    }
 }
 
-class Car extends Accelerate{
+class Car extends Vehicle {
     @Override
     public void accelerate(){
         speed++;
     }
 }
-class Bike extends Accelerate{
+class Bike extends Vehicle {
     @Override
     public void accelerate(){
         speed = speed +2;
