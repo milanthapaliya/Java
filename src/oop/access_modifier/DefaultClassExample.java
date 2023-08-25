01831e5343abbda48190ad72bd9f010afa9024c1
milanthapaliya <thapaliyamilan8@gmail.com>
2023-08-25 12:55:14 +0545

    added access modifiers

---
+++ src/oop/access_modifier/DefaultClassExample.java
@@ -0,0 +1,7 @@
+package oop.access_modifier;
+
+class DefaultClassExample {
+    public void show(){
+        System.out.println("This is a default Class. This can only be accessed from classed that are on the same package");
+    }
+}
---
+++ src/oop/access_modifier/example/AccessModifierExample.java
@@ -0,0 +1,58 @@
+package oop.access_modifier.example;
+
+//import oop.access_modifier.DefaultClassExample;
+
+public class AccessModifierExample {
+
+    public static void main(String[] args) {
+
+        // The line below will show error as default class cannot be accessed outside from its package
+       // DefaultClassExample defaultClass = new DefaultClassExample();   ---> ERROR
+
+        Student s = new Student("Student 1",  20, 1);
+        s.showDetails();
+        s.age = 25;
+        s.name = "Milan Thapaliya";
+        s.showDetails();
+
+    }
+}
+
+class Person{ // default class
+    String name; // default access modifier(accessible from same package)
+    protected int age; // protected access modifier(accessible from same package or subclass from other packages)
+    private int id; // private access modifier(accessible only from the class)
+    Person(){
+
+    }
+    Person(String name, int age, int id){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public void showId(){
+        System.out.println("ID is " + this.id);
+    }
+    public void showDetails(){
+        System.out.println("name = " + this.name + " age = " + this.age + " ID = " + this.id);
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+}
+class Student extends Person{
+    Student(String name, int age, int id){
+        this.name = name;
+        this.age = age;
+        this.setId(id);
+    }
+}
+
