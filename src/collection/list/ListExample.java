783d61b674f366e6bcfae556abf5c648c8d80f26
milanthapaliya <thapaliyamilan8@gmail.com>
2023-09-25 15:58:08 +0545

    some package refactors

--- src/collection/list/ListExample.java
+++ src/collection/list/ListExample.java
@@ -4,13 +4,12 @@ import java.util.*;
 
 public class ListExample {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(9);
         Iterator<Integer> iterator = list.iterator();
         while(iterator.hasNext()){
             System.out.println(iterator.next());
         }
-        Queue q = new PriorityQueue();
     }
 }
