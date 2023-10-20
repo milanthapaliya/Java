d4b514afd02db500337f5772e2f0aa08246e019f
milanthapaliya <thapaliyamilan8@gmail.com>
2023-10-20 11:38:58 +0545

    added recursive factorial example

--- src/recursion/RecursionExample.java
+++ src/recursion/RecursionExample.java
@@ -10,7 +10,29 @@ public class RecursionExample {
             printNumbersOneToTenRecursively(n + 1);
         }
     }
+    static public int factorial(int n){
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+        } else if (n == 0 || n == 1) {
+            return 1;
+        } else{
+            return n * factorial(n-1);
+        }
+    }
+    /*
+    factorial(5) = 5 * factorial(4)
+              = 5 * (4 * factorial(3))
+            = 5 * (4 * (3 * factorial(2)))
+            = 5 * (4 * (3 * (2 * factorial(1))))
+            = 5 * (4 * (3 * (2 * 1)))  // factorial(1) returns 1
+            = 5 * (4 * (3 * 2))
+            = 5 * (4 * 6)
+            = 5 * 24
+            = 120
+
+     */
     public static void main(String[] args) {
-        printNumbersOneToTenRecursively(1);
+//        printNumbersOneToTenRecursively(1);
+         System.out.println(factorial(5));
     }
 }
